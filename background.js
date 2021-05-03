82571f02c659e00aee273a8aa500b98b380d6d0e
Miftahul Arifin <miftahul97@gmail.com>
2021-05-03 09:51:32 +0700

    update view and fetch

--- background.js
+++ background.js
@@ -1,10 +1,35 @@
+function postData(api, data) {
+    return new Promise((resolve, reject) => {
+        fetch(api, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            resolve(response.json());
+        }).catch((error) => {
+            reject('Error : ', error);
+        });
+    });
+}
+
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-            console.log(request);
-            sendResponse({
-            	message: 'sent'
-        	});
+    const api = 'https://api-mirror.herokuapp.com';
+
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        if (request.function == 'postData') {
+            postData(api, {
+                ... request.data,
+                extension_id: sender.id
+            }).then((result) => {
+                console.log('result : ', result);
+            }).catch((error) => {
+                console.log(error);
+            });
         }
-    );
+        sendResponse({
+            message: 'sent'
+        });
+    });
 });
--- popup.js
+++ popup.js
@@ -4,27 +4,6 @@ function sendMessage(data) {
     });
 }
 
-async function postData(targetUrl, data = {}) {
-    var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    return new Promise((resolve, reject) => {
-        fetch(proxyUrl + targetUrl, {
-            method: 'POST',
-            mode: 'no-cors',
-            cache: 'no-cache',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then((response) => {
-            sendMessage({ok:true});
-            resolve(response.json());
-        }).catch((error) => {
-            sendMessage(error);
-            reject(error);
-        });
-    });
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     var sendButton      = document.getElementById('sendReport');
     var inputIssue      = document.getElementById('aligned-issue');
@@ -32,21 +11,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     sendButton.addEventListener('click', function() {
         chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-            var api = 'https://webhook.site/aa54cf0f-fe4d-41a6-9016-b63e28d79189';
-            postData(api, { 
-                url: tabs[0].url ?  tabs[0].url : '',
-                issue: inputIssue.value,
-                summary: inputSummary.value
-            }).then(data => {
-                sendMessage({
-                    status: 'done',
-                    message: data
-                });
-            }).catch(error => {
-                sendMessage({
-                    status: 'error',
-                    message: error
-                });
+            chrome.runtime.sendMessage({
+                function: 'postData',
+                data: {
+                    url: tabs[0].url ?  tabs[0].url : '',
+                    issue: inputIssue.value,
+                    summary: inputSummary.value
+                }
+            }, function(response) {
+                console.log(response);
             });
         });
     }, false);
