fe39c8537010009d332a8879084bbe23abcc3293
Miftahul Arifin <miftahul97@gmail.com>
2021-05-05 08:14:50 +0700

    add status success

--- background.js
+++ background.js
@@ -1,11 +1,27 @@
-function postData(api, data) {
+function countSuccessRate(api, extension_id, unresolved) {
     return new Promise((resolve, reject) => {
-        fetch(api, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
+        fetch(`${api}/report/unresolved?extension_id=${extension_id}&status=1`, {
+            method: 'GET'
+        }).then(async (response) => {
+            const resolved  = await response.json();
+            const reported  = resolved+unresolved;
+            const success   = unresolved ? Math.round(resolved*100*100/reported)/100 : 0;
+            resolve({
+                unresolved: unresolved,
+                resolved: resolved,
+                reported: reported,
+                success: `${success}%`
+            });
+        }).catch((error) => {
+            reject('Error : ', error);
+        });
+    });
+}
+
+function countUnresolved(api, extension_id) {
+    return new Promise((resolve, reject) => {
+        fetch(`${api}/report/unresolved?extension_id=${extension_id}&status=0`, {
+            method: 'GET'
         }).then((response) => {
             resolve(response.json());
         }).catch((error) => {
@@ -14,22 +30,93 @@ function postData(api, data) {
     });
 }
 
+function getData(api) {
+    return new Promise((resolve, reject) => {
+        fetch(`${api}/report`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            resolve(response.json());
+        }).catch((error) => {
+            reject('Error : ', error);
+        });
+    });
+}
+
+function postData(api, data) {
+    return new Promise((resolve, reject) => {
+        fetch(`${api}/report`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            if (response.status == 200) {
+                resolve(true);
+            } else {
+                reject(response.status);
+            }
+        }).catch((error) => {
+            reject('Error : ', error);
+        });
+    });
+}
+
 chrome.runtime.onInstalled.addListener(() => {
-    const api = 'https://api-mirror.herokuapp.com';
+    const api = 'http://localhost:8080';
 
     chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         if (request.function == 'postData') {
             postData(api, {
                 ... request.data,
+                status: 0,
                 extension_id: sender.id
             }).then((result) => {
                 console.log('result : ', result);
+                sendResponse({
+                    message: result
+                });
             }).catch((error) => {
                 console.log(error);
+                sendResponse({
+                    error: error
+                });
             });
+            return true;
+        } else if (request.function == 'getData') {
+            getData(api).then((result) => {
+                console.log('result : ', result);
+                sendResponse({
+                    message: result
+                });
+            }).catch((error) => {
+                console.log(error);
+                sendResponse({
+                    error: error
+                });
+            });
+            return true;
+        } else if (request.function == 'countUnresolved') {
+            countUnresolved(api, sender.id).then((unresolved) => {
+                countSuccessRate(api, sender.id, unresolved).then((result) => {
+                    console.log('result : ', result);
+                    sendResponse(result);
+                }).catch((error) => {
+                    console.log(error);
+                    sendResponse({
+                        unresolved: unresolved
+                    });
+                });
+            }).catch((error) => {
+                console.log(error);
+                sendResponse({
+                    unresolved: '-'
+                });
+            });
+            return true;
         }
-        sendResponse({
-            message: 'sent'
-        });
     });
 });
--- popup.js
+++ popup.js
@@ -1,13 +1,23 @@
-function sendMessage(data) {
-    chrome.runtime.sendMessage({ payload: data }, function(response) {
-        console.log(response);
-    });
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     var sendButton      = document.getElementById('sendReport');
     var inputIssue      = document.getElementById('aligned-issue');
     var inputSummary    = document.getElementById('aligned-summary');
+    var numUnresolved   = document.getElementById('num-unresolved');
+    var numResolved     = document.getElementById('num-resolved');
+    var numReported     = document.getElementById('num-reported');
+    var numSuccess      = document.getElementById('num-success');
+
+    chrome.runtime.sendMessage({
+        function: 'countUnresolved',
+        data: {}
+    }, function(response) {
+        if (response) {
+            numUnresolved.innerHTML = response.unresolved ? response.unresolved : '0';
+            numResolved.innerHTML   = response.resolved ? response.resolved : '0';
+            numReported.innerHTML   = response.reported ? response.reported : '0';
+            numSuccess.innerHTML    = response.success ? response.success : '0';
+        }
+    });
 
     sendButton.addEventListener('click', function() {
         chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
@@ -20,6 +30,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             }, function(response) {
                 console.log(response);
+                chrome.runtime.sendMessage({
+                    function: 'countUnresolved',
+                    data: {}
+                }, function(response) {
+                    if (response.count) {
+                        numUnreolved.innerHTML = response.count;
+                    }
+                });
             });
         });
     }, false);
